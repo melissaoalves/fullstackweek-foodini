c374554f873777428285c3f3746dc629e71ca6a1
melissaoalves <msnmelissaoa15@hotmail.com>
2025-02-19 21:17:25 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import simpleImportSort from "eslint-config-simple-import-sort";
 import { FlatCompat } from "@eslint/eslintrc";
+import { RuleTester } from "eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  }
 ];
 
 export default eslintConfig;
