945b0367222f349d4be69aeae5d82ae020489328
melissaoalves <msnmelissaoa15@hotmail.com>
2025-02-24 20:27:11 -0300

    feat: add restaurant menu page with categories and products

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,14 +1,42 @@
-import { Heading1 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import RestaurantHeader from "./components/header";
+import RestaurantCategories from "./components/categories";
 
 interface RestaurantMenuPageProps {
-    params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
 }
 
-const RestaurantMenuPage = async ({params}: RestaurantMenuPageProps) => {
-    const {slug} = await params
-    return ( 
-        <h1>{slug}</h1>
-    );
-}
- 
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: { menuCategories: { include: { products: true } } },
+  });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
+    </div>
+  );
+};
+
 export default RestaurantMenuPage;
